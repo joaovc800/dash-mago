cb1050f356876ea9bc61e7a2f853f6262a3460fe
João Vinicius da Costa <joaoviniciusdacosta800@gmail.com>
2024-09-10 08:27:13 -0300

    Criando links para o dashboard

--- app/views/dashboard.php
+++ app/views/dashboard.php
@@ -27,6 +27,15 @@ require_once realpath(dirname(__DIR__, 1) . '/controllers/checkSession.php');
                 <div class="dropdown-menu" id="dropdown-user" role="menu">
                     <div class="dropdown-content">
                         <a href="../../public/" class="dropdown-item is-danger is-flex is-align-items-center is-gap-1">
+                            Perfil
+                            <i class="fa-solid fa-user"></i>
+                        </a>
+                        <a href="../../public/" class="dropdown-item is-danger is-flex is-align-items-center is-gap-1">
+                            Operações
+                            <i class="fa-solid fa-cogs"></i>
+                        </a>
+                        <hr class="dropdown-divider" />
+                        <a href="../../public/" class="dropdown-item is-danger is-flex is-align-items-center is-gap-1">
                             Sair
                             <i class="fa-solid fa-right-from-bracket"></i>
                         </a>
@@ -34,42 +43,47 @@ require_once realpath(dirname(__DIR__, 1) . '/controllers/checkSession.php');
                 </div>
             </div>
         </div>
+        <div id="dashboard">
 
-        <h1 class="title">Carteira</h1>
-        
-        <div class="buttons">
-            <button class="button is-hoverable is-rounded has-text-light has-background-green-light">Inicio</button>
-            <button class="button is-hoverable is-rounded has-text-light">Configurações</button>
+            <h1 class="title">Carteira</h1>
+
+            <div class="buttons">
+                <button class="button is-hoverable is-rounded has-text-light has-background-green-light">Inicio</button>
+                <button class="button is-hoverable is-rounded has-text-light">Configurações</button>
+            </div>
+
+            <div class="pt-6">
+                <h1 class="title">Total</h1>
+
+                <div class="py-3 is-flex is-flex-direction-column is-gap-4">
+                    <div class="card is-flex is-flex-direction-column m-0 px-4 py-2 has-background-purple-dark has-text-light">
+                        <span>Valor total da banca</span>
+                        <span class="is-size-5 has-text-weight-bold">R$ 1.530,00</span>
+                    </div>
+
+                    <div class="card is-flex is-flex-direction-column m-0 px-4 py-2 has-background-purple-dark has-text-light">
+                        <span>Total de entradas</span>
+                        <span class="is-size-5 has-text-weight-bold">5</span>
+                    </div>
+
+                    <div data-profit="win" class="card is-flex is-flex-direction-column m-0 px-4 py-2 has-text-light">
+                        <span>Lucro total</span>
+                        <span class="is-size-5 has-text-weight-bold">R$ 500</span>
+                    </div>
+                </div>
+
+                <div class="buttons pt-5 is-flex is-justify-content-space-between">
+                    <a href="#" class="button is-hoverable is-rounded has-text-light">Tutoriais</a>
+                    <a href="#" class="button is-hoverable is-rounded has-text-light">Cadastrar</a>
+                </div>
+
+                <div class="is-flex pt-3">
+                    <button data-state="on" class="py-3 button is-fullwidth is-hoverable has-text-light has-text-weight-bold is-uppercase">Ligar</button>
+                </div>
+            </div>
         </div>
+        <div id="config">
 
-        <div class="pt-6">
-            <h1 class="title">Total</h1>
-
-            <div class="py-3 is-flex is-flex-direction-column is-gap-4">
-                <div class="card is-flex is-flex-direction-column m-0 px-4 py-2 has-background-purple-dark has-text-light">
-                    <span>Valor total da banca</span>
-                    <span class="is-size-5 has-text-weight-bold">R$ 1.530,00</span>
-                </div>
-
-                <div class="card is-flex is-flex-direction-column m-0 px-4 py-2 has-background-purple-dark has-text-light">
-                    <span>Total de entradas</span>
-                    <span class="is-size-5 has-text-weight-bold">5</span>
-                </div>
-
-                <div data-profit="win" class="card is-flex is-flex-direction-column m-0 px-4 py-2 has-text-light">
-                    <span>Lucro total</span>
-                    <span class="is-size-5 has-text-weight-bold">R$ 500</span>
-                </div>
-            </div>
-            
-            <div class="buttons pt-5 is-flex is-justify-content-space-between">
-                <button class="button is-hoverable is-rounded has-text-light">Tutoriais</button>
-                <button class="button is-hoverable is-rounded has-text-light">Cadastrar</button>
-            </div>
-
-            <div class="is-flex pt-3">
-                <button data-state="on" class="py-3 button is-fullwidth is-hoverable has-text-light has-text-weight-bold is-uppercase">Ligar</button>
-            </div>
         </div>
     </div>
 </body>
