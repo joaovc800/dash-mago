2760741947b991966510a661e73685143c680555
João Vinicius da Costa <joaoviniciusdacosta800@gmail.com>
2024-09-11 10:04:56 -0300

    novos dados

--- magoautomation.sql
+++ magoautomation.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 03/09/2024 às 14:28
+-- Tempo de geração: 11/09/2024 às 15:03
 -- Versão do servidor: 10.4.28-MariaDB
 -- Versão do PHP: 8.0.28
 
@@ -37,12 +37,19 @@ CREATE TABLE `magoautomation` (
   `stoplos` int(11) NOT NULL DEFAULT 0,
   `ficha` int(11) NOT NULL,
   `gale` int(11) NOT NULL DEFAULT 0,
-  `ligado` int(11) NOT NULL,
+  `status` varchar(20) NOT NULL,
   `noperacoes` int(11) NOT NULL DEFAULT 0,
   `ativo` int(11) NOT NULL DEFAULT 0,
   `assinatura` varchar(255) NOT NULL,
   `vencimento` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Despejando dados para a tabela `magoautomation`
+--
+
+INSERT INTO `magoautomation` (`id`, `email_login`, `senha_login`, `banca_inicial`, `banca_atual`, `stopwin`, `stoplos`, `ficha`, `gale`, `status`, `noperacoes`, `ativo`, `assinatura`, `vencimento`) VALUES
+(1, 'teste@teste.com', 'c20ad4d76fe97759aa27a0c99bff6710', 2102.00, 2500.30, 3000, 500, 1, 0, 'desligado', 6, 0, '1', '2024-09-30');
 
 --
 -- Índices para tabelas despejadas
@@ -62,7 +69,7 @@ ALTER TABLE `magoautomation`
 -- AUTO_INCREMENT de tabela `magoautomation`
 --
 ALTER TABLE `magoautomation`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
